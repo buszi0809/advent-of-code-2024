3ea5a88470b0771f3ecaa13552d91bf72b90e792
Kacper Wojciechowski <kacper.wojciechowski08090@gmail.com>
2024-12-03 20:17:22 +0100

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,8 @@
+fun main() {
+    println("1a: ${a1()}")
+    println("1b: ${b1()}")
+    println("2a: ${a2()}")
+    println("2b: ${b2()}")
+    println("3a: ${a3()}")
+    println("3b: ${b3()}")
+}
